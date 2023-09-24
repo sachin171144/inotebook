08ea52378d2fe4d53ec41cddd9c4b1a7de9d2c91
sachin171144 <sachingharabude@gmail.com>
2023-09-24 11:36:37 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,11 @@ connectToMongo();
 const app = express()
 const port = 5000
 
-app.use(cors())
+app.use(cors({
+  origin:[],
+  methods:['GET','POST','PUT','DELETE'],
+  credentials:true
+}))
 app.use(express.json())
 
 // Available Routes

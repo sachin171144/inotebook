fdf49ffc5c94b9184c17dfa9b23e6ba205117c33
sachin171144 <89479560+sachin171144@users.noreply.github.com>
2023-09-24 11:45:12 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express()
 const port = 5000
 
 app.use(cors({
-  origin:[],
+  origin:['https://inotebook-front.vercel.app'],
   methods:['GET','POST','PUT','DELETE'],
   credentials:true
 }))
